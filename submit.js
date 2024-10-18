345b777bf3d37a5faa1ffbbada245d3bc8d00672
mdsajidul <ai.mdsajidulbd@gmail.com>
2024-10-18 22:38:07 +0600

    Update submit.js

--- submit.js
+++ submit.js
@@ -10,16 +10,12 @@ document.addEventListener('DOMContentLoaded', () => {
         const password = document.getElementById('password').value;
         const confirmPassword = document.getElementById('confirm-password').value;
 
-        // Validate password and confirm password
-        if (password !== confirmPassword) {
-            alert("Passwords do not match.");
-            return;
-        }
+        
 
         // Create FormData object
         const formData = new FormData(signupForm);
 
-        fetch("https://script.google.com/macros/s/AKfycbxiiYuD0QAI8BkvMJoLUaCC2XXcOoI3BJkn3nUTDhNN-K5vRCC5Vxj0ciuZQcqNXC96/exec", {  // Replace "YOUR_WEB_APP_URL_HERE" with the actual Google Apps Script URL
+        fetch("https://script.google.com/macros/s/AKfycbxGufKEpmlehGVkrL0Q84NBZQEnmm4MpGYYTX5REGFNqgSrT7RY0bhBV03dhOsrFYUa/exec", {  // Replace "YOUR_WEB_APP_URL_HERE" with the actual Google Apps Script URL
             method: 'POST',
             body: formData,
         })
@@ -31,10 +27,10 @@ document.addEventListener('DOMContentLoaded', () => {
             })
             .then(data => {
                 if (data.result === "success") {
-                    alert("Sign Up successful!");
+                    alert("Submit successful!");
                     window.location.href = "index.html";// Redirect to `index.html`
                 } else {
-                    alert("Sign Up failed: " + (data.message || "Unknown error"));
+                    alert("Submit failed: " + (data.message || "Unknown error"));
                 }
             })
             .catch(error => {
